f61b53c52fcdb2642c9b7c64941380cdd75033e0
Edward Tanguay (wk) <edward@tanguay.info>
2020-08-12 17:23:57 +0200

    sass halfway doesn't work - fix sass

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 import config from './config';
-
 import express from 'express';
+import sassMiddleWare from 'node-sass-middleware';
+import path from 'path';
+
 const server = express();
 
 server.set('view engine', 'ejs');
 server.set('views', './views');
 
+server.use(sassMiddleWare({
+	src: path.join(__dirname, 'sass'),
+	dest: path.join(__dirname, 'public')
+}));
+
+
 server.get('/', (req, res) => {
 	res.render('index', {
-		content: '...' 
+		content: '...'
 	});
 });
 
